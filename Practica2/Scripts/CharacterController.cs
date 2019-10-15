c6bee13fc45b4b36f5d66a6ed1a7e51cf5095411
Daniel Suárez Labena <alu0101040882@ull.edu.es>
2019-10-15 19:47:55 +0100

    Practica 2 scripts

---
+++ Practica2/Scripts/CharacterController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour
+{
+
+    public float speed;
+
+    
+    void Start()
+    {
+        speed = 1;
+    }
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        
+        this.transform.position += movement*speed;
+
+
+    }
+
+
+}
